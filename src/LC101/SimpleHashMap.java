e1aa6adc068d64245aaef2d9732ed52253258683
GP <gangadharind@gmail.com>
2022-01-22 13:58:11 -0500

    Simple HashMap design.

---
+++ src/LC101/SimpleHashMap.java
@@ -0,0 +1,23 @@
+package LC101;
+
+import java.util.Arrays;
+
+public class SimpleHashMap {
+
+    int[] map;
+
+    public SimpleHashMap() {
+        Arrays.fill(map, -1);
+    }
+
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+    public int get(int key) {
+        return map[key];
+    }
+
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
