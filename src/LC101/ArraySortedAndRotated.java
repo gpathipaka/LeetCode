d3bec182c914afe4d82f2d03b2e614a5e3aa6027
GP <gangadharind@gmail.com>
2022-01-22 19:17:49 -0500

    Array Sorted and Rotated..

---
+++ src/LC101/ArraySortedAndRotated.java
@@ -0,0 +1,30 @@
+package LC101;
+
+public class ArraySortedAndRotated {
+
+    public boolean check(int[] arr) {
+        boolean rotated = false;
+        for(int i=0; i<arr.length -1; i++) {
+            if(arr[i] > arr[i+1]) {
+                if(rotated) {
+                    return false;
+                }
+                rotated = true;
+            }
+        }
+        if (rotated && arr[arr.length-1] > arr[0]) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ArraySortedAndRotated arraySortedAndRotated = new ArraySortedAndRotated();
+        int[] arr = { 3, 4, 5, 6 };
+        if(arraySortedAndRotated.check(arr)) {
+            System.out.println("Array Sorted and Roted");
+        } else {
+            System.out.println("************");
+        }
+    }
+}
